5733c35302faa4786f632dd0c5b79c10217737e2
Techboyzep <36669540+Techboyzep@users.noreply.github.com>
2021-06-17 17:03:56 +0530

    Button Fixed

---
+++ JS/read.js
@@ -0,0 +1,34 @@
+var scroll1 = window.pageYOffset;
+window.onscroll = function(){
+  var scroll2 = window.pageYOffset;
+  if (scroll1 > scroll2) {
+    document.querySelector("header").style.top ="0";
+  }else{
+    document.querySelector("header").style.top="-100px";
+  }
+  scroll1 =scroll2;
+}
+
+
+
+
+
+
+
+
+
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
