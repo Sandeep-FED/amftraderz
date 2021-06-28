853349462691a1c56a93741fb01d113ad3c7ac67
Sandeep-FED <sandymankara11@gmail.com>
2021-06-28 21:19:40 +0530

    nav bar responsive fixed

--- JS/read.js
+++ JS/read.js
@@ -9,6 +9,24 @@ window.onscroll = function(){
   scroll1 =scroll2;
 }
 
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
 
 
 function myFunction() {
